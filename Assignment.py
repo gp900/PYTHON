25aee1283a52e9b14fa5460c096bb81a83e2b777
gp900 <137711156+gp900@users.noreply.github.com>
2023-06-25 21:26:30 +0530

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,10 @@
+#Assignment 1
+#Define any 3 variables with given marks or user-defined marks and calculate average. Display the average in following format:
+#The average of n1,n2 and n3 is ___
+print("Average of 3 subject marks are ")
+sub1 = int(input("enter mark for sub1: "))
+sub2 = int(input("enter the mark for sub2: "))
+sub3 = int(input("enter the mark for sub3: "))
+num = sub1+sub2+sub3
+avg = num/3
+print("The Average of 3 subject are :",avg)
